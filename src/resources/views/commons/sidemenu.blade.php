aefb48bfc66f2a2eebd7b71c7337fc44d1ab95e8
shingo-suzumura <shingo.msdf@gmail.com>
2021-08-11 11:48:49 +0900

    サイドバー作成

--- src/resources/views/commons/sidemenu.blade.php
+++ src/resources/views/commons/sidemenu.blade.php
@@ -1,9 +1,16 @@
-<div class="col-2">
-    <div class="sidemenu">
-        <ul>
-            <li>
-                <a href="">マイページ</a>
+<div class="sidemenu mt-3 d-flex border">
+    <ul class="nav nav-pills flex-column">
+        @if(Auth::check())
+            <li class="nav-item mt-2">
+                <a class="nav-link text-dark" href="{{ route('camps.create') }}"><i class="fas fa-pencil-alt"></i>キャンプを投稿する</a>
             </li>
-        </ul>
-    </div>
+        @endif
+        <li class="nav-item mt-2">
+            <a class="nav-link text-dark" href="#"><i class="fab fa-free-code-camp"></i>キャンプを調べる</a>
+        </li>
+        <li class="nav-item mt-2">
+            <a class="nav-link text-dark" href="#"><i class="far fa-compass"></i>ギアを調べる</a>
+        </li>
+    </ul>
 </div>
+
