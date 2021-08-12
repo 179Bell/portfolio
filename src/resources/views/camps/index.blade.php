1d3c748bf182200668955c4bc720f29a435d0ebc
shingo-suzumura <shingo.msdf@gmail.com>
2021-08-12 15:14:39 +0900

    投稿カード・ドロップダウンリスト共通化

--- src/resources/views/camps/index.blade.php
+++ src/resources/views/camps/index.blade.php
@@ -2,20 +2,6 @@
 
 @section('content')
 
-    <!-- <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <form action="{{ route('camps.search') }}" method="POST">
-                    <div class="d-flex flex-row align-items-center">
-                        @csrf
-                        @method('GET')
-                        <input type="text" placeholder="キャンプ場名を入力してください" name="keyword" class="form-control">
-                        <button type="submit">検索</button>
-                    </div>
-                </form>
-            </div>
-        </div> 
-    </div> -->
     <div class="container mt-2">
         <div class="row">
             <div class="col-lg-9">
@@ -32,82 +18,12 @@
             </div>
         </div>
     </div>
-    
 
-        
     @foreach ( $camps as $camp )
         <div class="card mt-3">
-            <!-- カードヘッダー -->
-            <div class="card-header">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <div class="d-flex flex-row align-items-center">
-                                <a href="{{ route('users.profile', ['id' => $camp->user->id]) }}">
-                                    <img src="{{ asset('storage/images/'.$camp->user->avatar) }}" class="rounded-circle" width="90" height="90">
-                                </a>
-                                <h5 class="font-weight-bold ml-2">{{ $camp->user->name }}</h5>
-                                <p class="font-weight-lighter ml-2">{{ $camp->created_at->format('Y/m/d H:i') }}</p>
-                            </div>
-                        </div>
-                            @if (Auth::id() == $camp->user->id)
-                                <!-- ドロップダウン -->
-                                <div class="dropdown">
-                                    <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v"></i>
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" href="{{ route('camps.edit', ['camp' => $camp]) }}">
-                                            <i class="fas fa-pen mr-1"></i>編集する
-                                        </a>
-                                    <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $camp->id }}">
-                                            <i class="fas fa-trash-alt mr-1"></i>削除する
-                                        </a>
-                                    </div>
-                                </div>
-                                <!-- ドロップダウン -->
-                                <!-- モーダル -->
-                                <div id="modal-delete-{{ $camp->id }}" class="modal fade" tabindex="-1" role="dialog">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <form method="POST" action="{{ route('camps.destroy', ['camp' => $camp]) }}">
-                                                @csrf
-                                                @method('DELETE')
-                                                <div class="modal-body">
-                                                    投稿を削除します。よろしいですか？
-                                                </div>
-                                                <div class="modal-footer justify-content-between">
-                                                    <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                                                    <button type="submit" class="btn btn-danger">削除する</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endif
-                            <!-- モーダル -->
-
-                    </div>
-                </div>
-            </div>
-            <!--カードヘッダー-->
-
-            <div class="container">
-                <a style="text-decoration:none" href="{{ route('camps.show', ['camp' => $camp]) }}" class="text-dark">
-                    @include('commons.card')
-                </a>
-
-                @include('commons.button')
-            </div>
-        </div>
+            @include('commons.card')
+            @include('commons.button')
+        </div>       
     @endforeach
-
 
 @endsection('content')
--- src/resources/views/commons/card.blade.php
+++ src/resources/views/commons/card.blade.php
@@ -1,10 +1,38 @@
-<div class="card-body">
-    <h2>{{ $camp->title }}</h2>
-    @foreach ($camp->campImgs as $campImg)
-        <img src="{{ asset('storage/images/'.$campImg->img_path) }}" width="400" height="300">
-    @endforeach
+<!-- カードヘッダー -->
+<div class="card-header">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex flex-row align-items-center">
+                    <a href="{{ route('users.profile', ['id' => $camp->user->id]) }}">
+                        <img src="{{ asset('storage/images/'.$camp->user->avatar) }}" class="rounded-circle" width="90" height="90">
+                    </a>
+                    <h5 class="font-weight-bold ml-2">{{ $camp->user->name }}</h5>
+                    <p class="font-weight-lighter ml-2">{{ $camp->created_at->format('Y/m/d H:i') }}</p>
+                </div>
+            </div>
+                @if (Auth::id() == $camp->user->id)
+                    @include('commons.dropdown')
+                @endif
+        </div>
+    </div>
 </div>
-<div  class="card-body">
-    <p><i class="fas fa-map-marker-alt"></i>{{ $camp->location }}</p>
-    <p>{{ $camp->discription }}</p>
+<!--カードヘッダー-->
+<div class="container">
+    <a style="text-decoration:none" href="{{ route('camps.show', ['camp' => $camp]) }}" class="text-dark">
+        <div class="card-body">
+            <h2>{{ $camp->title }}</h2>
+            @foreach ($camp->campImgs as $campImg)
+                <img src="{{ asset('storage/images/'.$campImg->img_path) }}" width="400" height="300">
+            @endforeach
+        </div>
+        <div  class="card-body">
+            <p><i class="fas fa-map-marker-alt"></i>{{ $camp->location }}</p>
+            <p>{{ $camp->discription }}</p>
+        </div>
+    </a>
 </div>
+
+
+
+
