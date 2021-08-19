0cb599181a7c709ac25530b2828712a7f1373d12
shingo-suzumura <shingo.msdf@gmail.com>
2021-08-19 09:27:25 +0900

    ギア、コメントテスト

--- src/database/factories/CampFactory.php
+++ src/database/factories/CampFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Camp::class, function (Faker $faker) {
     return [
-                'user_id' => function() {
+            'user_id' => function() {
             return factory(App\User::class)->create()->id;
         },
         'date' => $faker->date($format='Y-m-d',$max='now'),
