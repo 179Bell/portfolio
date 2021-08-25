4bee3d8497f5d8e0fbf0b6c1cd332633bfafd3ea
shingo-suzumura <shingo.msdf@gmail.com>
2021-08-25 20:57:54 +0900

    CampServices作成

--- src/resources/views/camps/result.blade.php
+++ src/resources/views/camps/result.blade.php
@@ -10,7 +10,16 @@
             </div>
         @endforeach
     @else
-        <p>{{ $message }}</p>
+    <div class="card mt-5">
+        <div class="card-body d-flex row justify-content-center">
+            <h4>{{ $message }}</h4>
+        </div>
+    </div>
     @endisset
+    
+    <div class="row d-flex justify-content-center mt-5">
+        <a href="{{ route('top') }}" class="btn btn-secondary col-lg-6 btn-block">戻る</a>
+    </div>
+        
 
 @endsection('content')
