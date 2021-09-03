02bb046e418795cd711c1d29a64f5c95c646d707
shingo-suzumura <shingo.msdf@gmail.com>
2021-09-03 21:43:44 +0900

    MDBootstrap導入

--- src/resources/views/commons/footer.blade.php
+++ src/resources/views/commons/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-success mt-3">
     <div class="text-center">
-        <p class="text-light p-4">Shingo Suzumura All rights reserved.</p>
+        <p class="text-white p-4">Shingo Suzumura All rights reserved.</p>
     </div>
 </footer>
 
