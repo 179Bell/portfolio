8a43535db2bf7aa4444e5ddc0ea7da46511c9d34
shingo-suzumura <shingo.msdf@gmail.com>
2021-09-06 21:33:44 +0900

    WIP:welcomeページ作成

---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    @hasSection('title')
+        <title>@yield('title')</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
+</head>
+
+<nav class="navbar navbar-expand navbar-light bg-success border">
+    <p class="navbar-brand text-white mt-2"><i class="fas fa-campground"></i>ポートフォリオ</p>
+</nav>
+
+<body class="bg-white d-flex flex-column" style="min-height: 100vh">
+    <main class="mb-auto">
+            <div class="container-fluid">
+
+            </div>
+        </main>
+@include('commons.footer') 
+</body>
+
