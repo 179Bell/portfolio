5df0a8739c1941f4dc2dcd20ade405b639ab29b3
shingo-suzumura <shingo.msdf@gmail.com>
2021-09-08 22:35:36 +0900

    WIP:welcomページ画像追加

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <!-- Google Fonts -->
@@ -20,6 +22,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @hasSection('title')
         <title>@yield('title')</title>
     @else
@@ -34,7 +37,62 @@
 <body class="bg-white d-flex flex-column" style="min-height: 100vh">
     <main class="mb-auto">
             <div class="container-fluid">
+                <div class="row row-0"> 
+                    <div class="card" style="width: 100%">
+                        <img src="{{ asset('https://shingoportfolio.s3.ap-northeast-1.amazonaws.com/portfolio/IMG_1794.jpeg') }}" height="380px" > 
+                        <div class="card-img-overlay">
+                            <h1 class="text-white">さあ思い出を共有しよう</h1>
+                            <div class="btn btn-primary">
+                                <a class="text-white" href="{{ route('guest.login') }}">ゲストログイン</a>
+                            </div>
+                            <div class="btn btn-primary">
+                                <a class="text-white" href="{{ route('login') }}">ログイン</a>
+                            </div>
+                            <div class="btn btn-success">
+                                <a class="text-white" href="{{ route('register') }}">ユーザー登録はこちらから</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <h3 class="border-bottom">このアプリでできること</h3>
+                </div>
 
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <img src="{{ asset('https://shingoportfolio.s3.ap-northeast-1.amazonaws.com/portfolio/4271555_m.jpg') }}" height="300px" height="300px">
+                            </div>
+                            <div class="col-lg-5">
+                            <p>キャンツー仲間とつながろう</p>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-5">
+                            <div class="col-lg-5">
+                                <img src="{{ asset('https://shingoportfolio.s3.ap-northeast-1.amazonaws.com/portfolio/1897405_m.jpg') }}" height="300px" height="300px">
+                            </div>
+                            <div class="col-lg-5">
+                                <p>おすすめのギアを共有しよう！</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <h1>さあはじめよう！</h1>
+                        <div class="btn btn-primary">
+                            <a class="text-white" href="{{ route('guest.login') }}">ゲストログイン</a>
+                        </div>
+                        <div class="btn btn-primary">
+                            <a class="text-white" href="{{ route('login') }}">ログイン</a>
+                        </div>
+                        <div class="btn btn-success">
+                            <a class="text-white" href="{{ route('register') }}">ユーザー登録はこちらから</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
 @include('commons.footer') 
